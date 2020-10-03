1a616f6c556fd1f8012eb20799631f9d64e09fd9
Christopher Watkins <chriswatkins7@yahoo.com>
2020-10-03 21:18:51 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,25 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+phrase = "Hello World!" 
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+end
+
+say_hello_world_ten_times
+
+
+
+
+
+
